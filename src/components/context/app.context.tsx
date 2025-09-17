978c8bd2c7c0d966fa70cdf34f0ebbc179c66101
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-09-18 04:00:34 +0700

    fix dark theme

--- src/components/context/app.context.tsx
+++ src/components/context/app.context.tsx
@@ -16,7 +16,7 @@ export const AppContextProvider = ({
 }) => {
   const [theme, setTheme] = useState<ThemeContextType>(() => {
     const initialTheme =
-      localStorage?.getItem("theme") as ThemeContextType;
+      (localStorage?.getItem("theme") ?? "dark") as ThemeContextType;
     return initialTheme;
   });
 
